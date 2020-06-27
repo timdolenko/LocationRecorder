5685676e93537f778a8665cf175648d0d9bb4ec7
Tymofii Dolenko <sispo777@gmail.com>
2020-06-27 11:01:37 +0300

    Initial Commit

---
+++ Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge
+//
+//  Created by Tymofii Dolenko on 27.06.2020.
+//  Copyright © 2020 Tymofii Dolenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
